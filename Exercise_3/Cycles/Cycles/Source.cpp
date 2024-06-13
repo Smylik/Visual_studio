aeecd80926c3113206604b48d5afa227d61246f2
Smylik <kirill.mozin@gmail.com>
2024-06-13 23:43:04 +0300

    Corrections

--- Exercise_3/Cycles/Cycles/Source.cpp
+++ Exercise_3/Cycles/Cycles/Source.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 
 using namespace std;
+
+int choice(int num)
+{
+	int stuff = 12;
+	if (num==1)
+		for (int j = 1; j <= stuff; j++)
+			if (j%2!=0)
+				cout << "Odd: " << j << "\n";
+	if (num==2)
+		for (int j = 1; j <= stuff; j++)
+			if (j % 2 == 0)
+				cout << "Even: " << j << "\n";
+	 
+	return 0;
+	
+}
+
 int main()
 {
 	int stuff = 12;
-	for (int j = 1; j <= stuff; j++)
-		if (j % 2 == 0)
-			cout << "Even: " << j << "\n";
-		else
-			cout << "Odd: " << j << "\n";
+	int num;
+	cout << "Choose between 1-odd and 2-even: ";
+	cin >> num;
+	choice(num);
 	return 0;
 }
