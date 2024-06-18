1fee623779803b9426fab6abe747c7d3fa584caf
Smylik <kirill.mozin@gmail.com>
2024-06-18 20:44:35 +0300

    Class

---
+++ Exercise_5/Class/Class/Class.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Vector
+{
+private:
+	double x, y;
+public:
+	void input();
+	double length();
+};
+
+void Vector::input()
+{
+	cout << "Enter x: " << endl;
+	cin >> x;
+	cout << "Enter y: " << endl;
+	cin >> y;
+}
+
+double Vector::length()
+{
+	cout << "x=" << x << "\n" << "y=" << y << endl;
+	double result = sqrt(pow(x, 2) + pow(y, 2));
+	return result;
+}
+
+int main()
+{
+	Vector out;
+	out.input();
+	cout << "Result: " << out.length() << endl;
+	return 0;
+}
+
+
